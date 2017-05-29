83b52a25f684eaf8037053549759e6895e768f44
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-29 11:11:30 -0500

    insercion de datos base de datos kimberly

--- base de datos kimberly.sql
+++ base de datos kimberly.sql
@@ -17,3 +17,20 @@ CREATE TABLE item (
     FOREIGN KEY (made_of)
         REFERENCES material (material_id)
 );
+insert into material(material_id,descr,cost,density)values
+('ME','Metal','9.87','5.52'),
+('PA','Paper','1.50','0.57'),
+('PL','Plastic','2.50','1.82'),
+('WL','Wool','7.50','0.28'),
+('WO','Wood','3.53','0.79');
+insert into item(name,heigth,width,depth,made_of,in_stock)values
+('Bed','1','12','9','WL',121),
+('Book','3','2','0.5','Pa',50),
+('CD','5','6','0.5','PL',58),
+('Chair','8','4','4','WO',77),
+('Computer','2','1','11','ME',553),
+('Picture','10','12','0.1','PA',92),
+('Printer','2.2','1.6','1.4','PL',5),
+('Speaker','2','1.5','4.7','WO',145),
+('Table','12','3','4','WO',12),
+('TV','2','7','5','PL',100);
